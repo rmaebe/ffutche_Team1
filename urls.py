ad57c8d70ea1d5441379c4056c914170255efc39
rmaebe <56825810+rmaebe@users.noreply.github.com>
2019-11-21 21:04:06 -0700

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,29 @@
+"""Django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from ffutche import views
+
+urlpatterns = [
+    path('', views.welcome, name='welcome'),
+    path('Website.html', views.welcome, name='welcome'),
+    path('Signin.html', views.signin, name='Sign_in'),
+    path('JoinUs.html', views.register, name='registration'),
+    path('webportal.html', views.portal, name='web_portal'),
+    #...
+    path('api/', include('ffutche.api.url', namespace='api')),
+]
